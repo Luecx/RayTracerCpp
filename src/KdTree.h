596790639a952fac2152a216950dd8e373e95783
Finn Eggers <web@finneggers.de>
2021-02-04 18:15:45 +0100

    fixed some triangle construction

--- src/KdTree.h
+++ src/KdTree.h
@@ -7,23 +7,22 @@
 
 
 #include <vector>
-
 struct KdNode{
 
-//    std::vector<Triangle> triangles;
+    std::vector<Triangle> triangles{};
 //    KdNode* left  = nullptr;
 //    KdNode* right = nullptr;
-//
-//    KdNode(std::vector<Triangle> triangles, int depth){
-//        if(depth == 0){
-//            this->triangles = triangles;
-//        }else{
-//            left  = new KdNode(triangles, depth-1);
-//            right = new KdNode(triangles, depth-1);
-//        }
-//    }
-//
-//    virtual ~KdNode() {
+
+    KdNode(std::vector<Triangle> p_triangles, int depth){
+        if(depth == 0){
+            this->triangles.insert(this->triangles.end(), p_triangles.begin(), p_triangles.end());
+        }else{
+////            left  = new KdNode(triangles, depth-1);
+////            right = new KdNode(triangles, depth-1);
+        }
+    }
+
+    virtual ~KdNode() {
 //        if(left != nullptr){
 //            delete left;
 //            left = nullptr;
@@ -32,7 +31,7 @@ struct KdNode{
 //            delete right;
 //            right = nullptr;
 //        }
-//    }
+    }
 
 };
 
